e8ec90360cba9bdb1df19afdbd1e6b26ec6992de
xiaol <liuxiao.in@gmail.com>
2023-07-19 12:34:00 +0800

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,10 +1,10 @@
 export const StarRingAPIEndpoint = 'https://staring-api.xiaomiqiu.com/api/chat_base' ;
 
-export const RWKVAPIEndpoint = 'https://rwkv.ai-creator.net/chat/completions' ;
+export const RWKVAPIEndpoint = 'https://rwkv.ai-creator.net/jpntuned/v1/chat/completions' ;
 
 export const RWKVCHNAPIEndpoint = 'https://rwkv.ai-creator.net/chntuned/v1/chat/completions' ;
 
-export const RWKVJPNAPIEndpoint = 'https://rwkv.ai-creator.net/jpntuned/v1/chat/completions' ;
+export const RWKVJPNAPIEndpoint = 'https://rwkv.ai-creator.net/chat/completions' ;
 //可用得端点
 
 export const availableEndpoints = [RWKVJPNAPIEndpoint,RWKVCHNAPIEndpoint,RWKVAPIEndpoint];
